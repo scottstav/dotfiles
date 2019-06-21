da947035483b5943e4f3a5652e2f117265c5905b
scottstav 🌿 <howdy@scotty.dance>
2019-06-20 22:34:04 -0700

    md package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,12 @@
+;; Add MELPA
 
+(require 'package)
+(require 'use-package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+
+;; Save Configuration ;;
 (setq backup-directory-alist `(("." . "~/.saves")))
 
 (setq backup-by-copying t)
@@ -8,3 +16,28 @@
       kept-old-versions 0   ;; Number of oldest versions to keep.
       delete-old-versions t ;; Don't ask to delete excess backup versions.
       backup-by-copying t)  ;; Copy all files, don't rename them.
+;; ------------------ ;;
+
+
+;; Markdown
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (use-package markdown-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

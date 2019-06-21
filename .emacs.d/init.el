4c43901b1bbf25f6f2b1d6b38142ff538512f3ad
scottstav 🌿 <howdy@scotty.dance>
2019-06-20 21:41:24 -0700

    emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,10 @@
+
+(setq backup-directory-alist `(("." . "~/.saves")))
+
+(setq backup-by-copying t)
+
+(setq version-control t     ;; Use version numbers for backups.
+      kept-new-versions 10  ;; Number of newest versions to keep.
+      kept-old-versions 0   ;; Number of oldest versions to keep.
+      delete-old-versions t ;; Don't ask to delete excess backup versions.
+      backup-by-copying t)  ;; Copy all files, don't rename them.

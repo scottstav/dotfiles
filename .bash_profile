4c799c0b81a7165f34ead9958d4d18985fc3a3d8
scottstav 🌿 <howdy@scotty.dance>
2019-08-09 11:51:32 -0700

    emacs web-mode + git completion

--- .bash_profile
+++ .bash_profile
@@ -2,4 +2,14 @@
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+
 [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
+# Initialize bash completion
+[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion || {
+    # if not found in /usr/local/etc, try the brew --prefix location
+    [ -f "$(brew --prefix)/etc/bash_completion.d/git-completion.bash" ] && \
+        . $(brew --prefix)/etc/bash_completion.d/git-completion.bash
+}
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,5 @@
 ;; Add MELPA
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
@@ -6,6 +7,7 @@
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+
 
 ;; Save Configuration ;;
 (setq backup-directory-alist `(("." . "~/.saves")))
@@ -20,7 +22,25 @@
 ;; ------------------ ;;
 
 
-;; Markdown
+;; language config
+(unless (package-installed-p 'inf-ruby)
+  (package-install 'inf-ruby))
+(add-hook 'json-mode-hook
+          (lambda ()
+            (make-local-variable 'js-indent-level)
+            (setq js-indent-level 2)))
+
+(defun web-mode-init-hook ()
+  "Hooks for web-mode"
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-markup-indent-offset 2))
+
+(add-hook 'web-mode-hook  'web-mode-init-hook)
+(setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
+(add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css?$" . web-mode))
+
 (use-package markdown-mode
   :ensure t
   :commands (markdown-mode gfm-mode)
@@ -30,12 +50,21 @@
   :init (setq markdown-command "multimarkdown"))
 (setq markdown-command "pandoc")
 
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(ido-mode 1)
+(when (version<= "26.0.50" emacs-version )
+  (global-display-line-numbers-mode))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (groovy-mode gradle-mode use-package markdown-mode))))
+ '(package-selected-packages
+   (quote
+    (inf-ruby flycheck web-mode json-mode groovy-mode gradle-mode use-package markdown-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

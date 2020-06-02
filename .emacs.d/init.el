b677b4c9432882490ee3ef14901f42228a31dba4
scottstav <howdy@scotty.dance>
2020-06-01 22:48:14 -0500

    mail

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,9 +1,22 @@
 ;; Add MELPA
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu/mu4e")
 (package-initialize)
 
 (add-to-list 'load-path "~/.emacs.d/lisp/")
+
+;; MAIL
+(require 'mu4e)
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+(setq mu4e-mu-binary (executable-find "mu"))
+;; enable inline images
+(setq mu4e-view-show-images t)
+;; use imagemagick, if available
+(when (fboundp 'imagemagick-register-types)
+  (imagemagick-register-types))
+;; END MAIL
 
 ;; dont remember what this was for...
 (eval-after-load 'gnutls
@@ -18,7 +31,105 @@
   (let ((dired-listing-switches "-alh"))
     (dired-other-window buffer-file-name)))
 
+;; (use-package treemacs
+;;   :ensure t
+;;   :defer t
+;;   :init
+;;   (with-eval-after-load 'winum
+;;     (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+;;   :config
+;;   (progn
+;;     (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
+;;           treemacs-deferred-git-apply-delay      0.5
+;;           treemacs-directory-name-transformer    #'identity
+;;           treemacs-display-in-side-window        t
+;;           treemacs-eldoc-display                 t
+;;           treemacs-file-event-delay              5000
+;;           treemacs-file-extension-regex          treemacs-last-period-regex-value
+;;           treemacs-file-follow-delay             0.2
+;;           treemacs-file-name-transformer         #'identity
+;;           treemacs-follow-after-init             t
+;;           treemacs-git-command-pipe              ""
+;;           treemacs-goto-tag-strategy             'refetch-index
+;;           treemacs-indentation                   2
+;;           treemacs-indentation-string            " "
+;;           treemacs-is-never-other-window         nil
+;;           treemacs-max-git-entries               5000
+;;           treemacs-missing-project-action        'ask
+;;           treemacs-move-forward-on-expand        nil
+;;           treemacs-no-png-images                 nil
+;;           treemacs-no-delete-other-windows       t
+;;           treemacs-project-follow-cleanup        nil
+;;           treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+;;           treemacs-position                      'left
+;;           treemacs-recenter-distance             0.1
+;;           treemacs-recenter-after-file-follow    nil
+;;           treemacs-recenter-after-tag-follow     nil
+;;           treemacs-recenter-after-project-jump   'always
+;;           treemacs-recenter-after-project-expand 'on-distance
+;;           treemacs-show-cursor                   nil
+;;           treemacs-show-hidden-files             t
+;;           treemacs-silent-filewatch              nil
+;;           treemacs-silent-refresh                nil
+;;           treemacs-sorting                       'alphabetic-asc
+;;           treemacs-space-between-root-nodes      t
+;;           treemacs-tag-follow-cleanup            t
+;;           treemacs-tag-follow-delay              1.5
+;;           treemacs-user-mode-line-format         nil
+;;           treemacs-user-header-line-format       nil
+;;           treemacs-width                         35)
+
+;;     ;; The default width and height of the icons is 22 pixels. If you are
+;;     ;; using a Hi-DPI display, uncomment this to double the icon size.
+;;     ;;(treemacs-resize-icons 44)
+
+;;     (treemacs-follow-mode t)
+;;     (treemacs-filewatch-mode t)
+;;     (treemacs-fringe-indicator-mode t)
+;;     (pcase (cons (not (null (executable-find "git")))
+;;                  (not (null treemacs-python-executable)))
+;;       (`(t . t)
+;;        (treemacs-git-mode 'deferred))
+;;       (`(t . _)
+;;        (treemacs-git-mode 'simple))))
+;;   :bind
+;;   (:map global-map
+;;         ("M-0"       . treemacs-select-window)
+;;         ("C-x t 1"   . treemacs-delete-other-windows)
+;;         ("C-x t t"   . treemacs)
+;;         ("C-x t B"   . treemacs-bookmark)
+;;         ("C-x t C-t" . treemacs-find-file)
+;;         ("C-x t M-t" . treemacs-find-tag)))
+
+
+;; (use-package treemacs-projectile
+;;   :after treemacs projectile
+;;   :ensure t)
+
+;; (use-package treemacs-icons-dired
+;;   :after treemacs dired
+;;   :ensure t
+;;   :config (treemacs-icons-dired-mode))
+
+;; (use-package treemacs-magit
+;;   :after treemacs magit
+;;   :ensure t)
+
+;; (use-package treemacs-persp
+;;   :after treemacs persp-mode
+;;   :ensure t
+;;   :config (treemacs-set-scope-type 'Perspectives))
+
 (global-set-key (kbd "C-x C-b") 'ibuffer)
+
+;; scroll one line at a time (less "jumpy" than defaults)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+
+(setq scroll-step 1) ;; keyboard scroll one line at a time
 
 (dolist (package '(desktop+ google-this))
   (unless (package-installed-p package)
@@ -42,11 +153,12 @@
 		   (interactive)
 		   (popup-menu 'yank-menu)))
 
-
-
 ;; org mode
 (require 'org)
-
+;;store org-mode links to messages
+(require 'org-mu4e)
+;;store link to message if in header view, not to header query
+(setq org-mu4e-link-query-in-headers-mode nil)
 (setq org-directory "~/Dropbox/org")
 (setq org-default-notes-file (concat org-directory "/General.org"))
 (global-set-key (kbd "C-c l") 'org-store-link)
@@ -69,11 +181,15 @@
         (goto-line -1))))
 
 (setq org-capture-templates
-  '(
-    ("j" "Journal Entry"
-     entry (function org-capture-journal-location))
-    )
-  )
+      `(
+	("j" "Journal Entry"
+	 entry (function org-capture-journal-location))
+	("t" "Todo"
+	 entry (file ,(concat org-directory "/General.org"))
+	 "* TODO %?\n  %iSCHEDULED: %^t\n  %a")
+      	)
+      )
+
 ;;------------------language config------------------------------
 
 (show-paren-mode 1)
@@ -136,9 +252,13 @@
 
 ;;---------------------------------------------------------------
 
-(defun connect-remote ()
+(defun connect-remote-minecraft ()
   (interactive)
   (dired "/ec2-user@minecraft.scotty.dance:~"))
+
+(defun connect-remote-arch ()
+  (interactive)
+  (dired "/root@10.0.1.41:/"))
 
 ;; Shell
   (defun with-face (str &rest face-plist)
@@ -210,10 +330,10 @@
  '(custom-enabled-themes (quote (wombat)))
  '(org-agenda-files
    (quote
-    ("~/Dropbox/org/General.org" "~/Dropbox/org/movement.org" "~/Dropbox/org/shopping.org" "/Users/scottstav/Dropbox/org/dotfiles.org" "/Users/scottstav/Dropbox/org/poetry.org" "/Users/scottstav/Dropbox/org/posts.org" "/Users/scottstav/Dropbox/org/projects.org" "/Users/scottstav/Dropbox/org/sensunDnD.org")))
+    ("~/Dropbox/org/birthdays.org" "~/Dropbox/org/General.org" "~/Dropbox/org/movement.org" "~/Dropbox/org/shopping.org" "/Users/scottstav/Dropbox/org/dotfiles.org" "/Users/scottstav/Dropbox/org/poetry.org" "/Users/scottstav/Dropbox/org/posts.org" "/Users/scottstav/Dropbox/org/projects.org" "/Users/scottstav/Dropbox/org/sensunDnD.org")))
  '(package-selected-packages
    (quote
-    (elpy exec-path-from-shell google-this desktop+ magit git js2-mode flymake-ruby robe inf-ruby flycheck web-mode json-mode groovy-mode gradle-mode use-package markdown-mode))))
+    (treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs elpy exec-path-from-shell google-this desktop+ magit git js2-mode flymake-ruby robe inf-ruby flycheck web-mode json-mode groovy-mode gradle-mode use-package markdown-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

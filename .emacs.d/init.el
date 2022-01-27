bf77c848fd114caca2ed4522161bda62694ed0ba
scottstav <scottstavinoha@gmail.com>
2022-01-27 12:24:56 -0800

    better help buffers with

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -28,7 +28,6 @@
 				     (centered-cursor-mode -1)
 				     )))
 
-
 ;; random annoying message
 (setq byte-compile-warnings '(cl-functions))
 
@@ -700,6 +699,15 @@ Including indent-buffer, which should not be called automatically on save."
   :ensure)
 (which-key-mode 1)
 
+(use-package helpful
+  :ensure
+  :config
+  (global-set-key (kbd "C-h f") #'helpful-callable)
+  (global-set-key (kbd "C-h v") #'helpful-variable)
+  (global-set-key (kbd "C-h k") #'helpful-key)
+  (global-set-key (kbd "C-c C-d") #'helpful-at-point)
+  )
+
 (use-package key-chord
   :ensure)
 (key-chord-mode 1)
@@ -796,3 +804,17 @@ Assume point is in the corresponding edit buffer."
 (setq global-display-line-numbers-mode t)
 (setq markdown-command "/usr/local/bin/pandoc")
 (setq global-visual-line-mode t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(helpful zoom yascroll yaml-mode which-key vterm use-package tide spaceline solaire-mode smex smart-mode-line simple-modeline robe restclient rainbow-mode prettier-js paredit org-roam-ui org-pomodoro org-jira orderless ob-mongo ob-http ob-graphql oauth2 nodejs-repl multiple-cursors mood-line modus-themes lsp-ui lsp-sourcekit key-chord jest-test-mode ivy-posframe impatient-mode helm-xref helm-swoop helm-projectile helm-lsp helm-dash helm-c-yasnippet helm-ag git-gutter+ forge flymake-ruby fancy-battery expand-region exec-path-from-shell emojify elpy doom-themes doom-modeline dashboard counsel-projectile centered-cursor-mode browse-at-remote ace-window ace-jump-mode))
+ '(zoom-size '(0.618 . 0.618)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

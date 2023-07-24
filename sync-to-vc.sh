eb31ccad5a38c3fbb9fb48df23d77369b5d616d8
scottstav <scottstavinoha@gmail.com>
2023-07-24 11:50:37 -0500

    sync-to-vc 😳

--- sync-to-vc.sh
+++ sync-to-vc.sh
@@ -1,30 +1,14 @@
 #!/bin/bash
 
-# This script does not work properly yet
-# The `files-to-sync` needs to be modified to exclude directories, e.g. `.emacs.d` should instead be `.emacs.d/init.el`
-
 cd "$(dirname "${BASH_SOURCE}")";
 
-ls -A > files-to-sync
-
 function doIt() {
-    rsync --files-from=files-to-sync \
-          --exclude ".git/" \
-          --exclude "bootstrap.sh" \
-          --exclude "sync-to-vc.sh" \
-          --exclude "README.md" \
+    find . -path ./.git -prune -o -type f \( -not -name "bootstrap.sh" -and -not -name "README.md" -and -not -name "sync-to-vc.sh" \) -print > ./.git/files-to-sync
+    rsync --files-from=./.git/files-to-sync \
           -ar ~ .;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-    doIt;
-else
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-    echo "";
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        doIt;
-    fi;
-fi;
+doIt;
 unset doIt;
 
-rm files-to-sync
+rm ./.git/files-to-sync
